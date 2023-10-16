9dfc18580a3f9a6ea8b1f5e14173433cbd25bed9
Rasika Samith <rasikasamith@gmail.com>
2023-10-17 00:04:17 +0530

    Added FreelancerUI
    Added FreelancerUI to the repository

---
+++ FreelancerUI/FreelancerUI/Models/UserViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FreelancerUI.Models
+{
+	public class UserViewModel
+	{
+		public int UId { get; set; }
+
+		[Required(ErrorMessage = "User Name is required")]
+		public string UserName { get; set; }
+
+        [Required(ErrorMessage ="Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+		public string PhoneNumber { get; set; }
+		        
+        [Required(ErrorMessage = "Skill set is required")]
+        public string SkilSets { get; set; }
+
+		public string Hobby { get; set; }
+	}
+}
